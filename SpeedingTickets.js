b9f0f9f5fea7c3c10b445de9b45df70dba1f6d7f
CrazyRabbitZheng <98835462+CrazyRabbitZheng@users.noreply.github.com>
2022-10-26 20:55:12 -0230

    Update SpeedingTickets.js

--- SpeedingTickets.js
+++ SpeedingTickets.js
@@ -1,5 +1,5 @@
 //speed limit is 70 km/h
-//every 5 km above 75 demerit 1 point
+//every 5 km above 70 demerit 1 point
 //say 73 km/h, does not over 70 by 5;dont demerit point
 //if demerit 12 points, the DL gets suspended
 //create a function called checkSpeed 
